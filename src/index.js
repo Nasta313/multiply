e5bf70467280278cd55830366a591db422fa446e
Nasta Vlasovets <vlasovec.nasta313@gmail.com>
2019-09-23 12:14:08 +0300

    feat: the implementation of the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstReverseArr = first.split('').reverse();
+  let secondReverseArr = second.split('').reverse();
+
+  let stack = [];
+
+  for (let i = 0; i < firstReverseArr.length; i++) {
+    for (let j = 0; j < secondReverseArr.length; j++) {
+      let prodactOfNumbers = firstReverseArr[i] * secondReverseArr[j];
+      if(stack[i + j]){
+        stack[i + j] = stack[i + j] + prodactOfNumbers;
+      } else {
+        stack[i + j]= prodactOfNumbers;
+      } 
+    }
+  }
+
+  for (let i = 0; i < stack.length; i++) {
+    let num = stack[i] % 10;
+    let move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1]){
+      stack[i + 1] += move;
+    } else if (move != 0){
+      stack[i + 1] = move;
+    }
+  }
+
+  return stack.reverse().join('');
 }
+
